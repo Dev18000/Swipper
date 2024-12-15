0c1e2f09854bc5de69bca684a9eca1e74d098aba
farida <dev18@laposte.net>
2024-12-15 10:22:52 +0100

    Добавьте файлы проекта.

---
+++ Swipper/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿window.initializeSwiper = () => {
+    new Swiper('.swiper-container', {
+        direction: 'vertical', // Вертикальный режим
+        mousewheel: true,     // Поддержка колесика мыши
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: false,          // Отключить бесконечную прокрутку, если не требуется
+    });
+};
